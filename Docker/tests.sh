8c610abaf2d37ca37886ce5180a2a8fc5cb94316
Ayoub Ben hamou <ayoubbenhamou0731@gmail.com>
2024-12-10 10:56:52 +0100

    Add GitHub Actions workflow to run automated tests and 2D domain construction tests

---
+++ Docker/tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pushd manapy/tests
+mpirun --allow-run-as-root --use-hwthread-cpus -n 2 python3 mpi_test.py
+mpirun --allow-run-as-root --use-hwthread-cpus -n 4 --oversubscribe python3 mpi_test.py
+
+popd
+
+pushd manapy/examples/2D
+python3 -c "import manapy; import manapy.ddm; print(manapy.__version__)"
+python3 advection2d.py
+mpirun --allow-run-as-root --use-hwthread-cpus -n 2 python3 advection2d.py
+mpirun --allow-run-as-root --use-hwthread-cpus -n 4 --oversubscribe python3 advection2d.py #oversubscribe option allows you to run more MPI processes than there are available CPU cores on the system.
