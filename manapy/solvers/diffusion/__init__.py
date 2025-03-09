680515018fb7c2f9dba63aeed58537f2da264cf7
imadki <kissami.imad@gmail.com>
2025-03-09 12:18:07 +0000

    add diffusion solver

---
+++ manapy/solvers/diffusion/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+
+from manapy.solvers.diffusion.fvm_utils import *
+from manapy.solvers.diffusion.tools_utils import *
+from manapy.solvers.diffusion.system import DiffusionSolver
