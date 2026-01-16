54ae778f006a9644455812462376960ed84ad928
Ayoub ben hamou <ayoubbenhamou0731@gmail.com>
2026-01-16 12:41:53 +0100

    domain

--- tests/ginkgo/1.cpp
+++ tests/ginkgo/1.cpp
@@ -18,6 +18,7 @@ using IndexType = int;
 using mtx = gko::matrix::Csr<ValueType, IndexType>;
 using vec = gko::matrix::Dense<ValueType>;
 using real_vec = gko::matrix::Dense<ValueType>;
+using mg = gko::solver::Multigrid;
 
 double compute_residual(const std::shared_ptr<gko::Executor>& exec, const std::shared_ptr<mtx>& A, const std::shared_ptr<vec> b, const std::shared_ptr<vec> x)
 {
@@ -31,7 +32,7 @@ double compute_residual(const std::shared_ptr<gko::Executor>& exec, const std::s
 }
 
 template <typename Solver>
-void solve_system(const std::shared_ptr<gko::Executor>& exec, const std::shared_ptr<mtx>& A, const std::shared_ptr<vec> b, const std::shared_ptr<vec> x)
+void solve_system(const std::shared_ptr<gko::Executor>& exec, const std::shared_ptr<mtx>& A, const std::shared_ptr<vec> &b, const std::shared_ptr<vec> &x)
 {
     // #####################################################
     // Preconditioners
@@ -77,8 +78,8 @@ void solve_system(const std::shared_ptr<gko::Executor>& exec, const std::shared_
     //using solver = gko::solver::Gmres<ValueType>; // Gmres, Cgs, Bicg
 
     std::shared_ptr<gko::LinOpFactory> solver_factory = Solver::build()
-        .with_criteria(residual_criteria)
-        .with_generated_preconditioner(ic_pre_factory_precon)
+        .with_criteria(residual_criteria, iteration_criteria)
+        //.with_generated_preconditioner(ic_pre_factory_precon)
         .on(exec);
 
     // Generate preconditioned solver for a specific target system
@@ -99,7 +100,75 @@ void solve_system(const std::shared_ptr<gko::Executor>& exec, const std::shared_
 
 }
 
-
+// void solve_system_multigrid(const std::shared_ptr<gko::Executor>& exec, const std::shared_ptr<mtx>& A, const std::shared_ptr<vec> &b, const std::shared_ptr<vec> &x, const IndexType max_levels) {
+//
+//     // Create multigrid factory
+//     std::shared_ptr<gko::LinOpFactory> multigrid_gen;
+//     multigrid_gen =
+//         mg::build()
+//             .with_mg_level(pgm::build().with_deterministic(true))
+//             .with_criteria(gko::stop::Iteration::build().with_max_iters(1u))
+//             .on(exec);
+//     const gko::remove_complex<ValueType> tolerance = 1e-8;
+//     auto solver_gen =
+//         cg::build()
+//             .with_criteria(gko::stop::Iteration::build().with_max_iters(100u),
+//                            gko::stop::ResidualNorm<ValueType>::build()
+//                                .with_baseline(gko::stop::mode::absolute)
+//                                .with_reduction_factor(tolerance))
+//             .with_preconditioner(multigrid_gen)
+//             .on(exec);
+//
+//
+//     auto mg_solver_factory = gko::solver::Multigrid::build()
+//         // Number of AMG levels (coarser → faster)
+//         .with_max_levels(10)
+//
+//         // Choose smoother: Jacobi is safest, ParILU is stronger
+//         .with_pre_smoother(gko::share(
+//             gko::preconditioner::Jacobi<ValueType, IndexType>::build().on(exec)
+//         ))
+//         .with_post_smoother(gko::share(
+//             gko::preconditioner::Jacobi<ValueType, IndexType>::build().on(exec)
+//         ))
+//
+//         // Coarse-level solver (often CG or direct LU)
+//         .with_coarsest_solver(gko::share(
+//             gko::solver::Cg<ValueType>::build().on(exec)
+//         ))
+//
+//         // // Coarsening (Ruge–Stüben)
+//         // .with_coarsening(
+//         //     gko::share(gko::multigrid::CoarseningOperator::build().on(exec))
+//         // )
+//
+//
+//         .with_criteria(
+//             //gko::stop::Iteration::build().with_max_iters(gko::size_type{1000}),
+//             gko::stop::ResidualNorm<ValueType>::build().with_reduction_factor(1e-8)
+//         )
+//
+//         .on(exec);
+//
+//     // Generate the multigrid hierarchy using A
+//     auto mg_solver = mg_solver_factory->generate(A);
+//
+//     // ###############################
+//     //    SOLVE Ax = b
+//     // ###############################
+//
+//     auto logger = gko::share(gko::log::Convergence<>::create());
+//     mg_solver->add_logger(logger);
+//
+//     exec->synchronize();
+//     auto time_start = std::chrono::steady_clock::now();
+//     mg_solver->apply(b, x);
+//     exec->synchronize();
+//     auto time_end = std::chrono::steady_clock::now();
+//     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(time_end - time_start);
+//     std::cout << "Iterations: " << logger->get_num_iterations() << "\n";
+//     std::cout << "Time: " << duration.count() << " microseconds" << "\n";
+// }
 
 
 int main(int argc, char* argv[])
@@ -111,7 +180,7 @@ int main(int argc, char* argv[])
     if (argc != 4) {
         std::cerr << "Usage: " << argv[0] << " [executor]" << " [solver name]" << " [data folder containing A,b,solution]" << std::endl;
         std::cerr << "executor: omp, cuda, hip, dpcpp, reference" << std::endl;
-        std::cerr << "sover name: bicg ,bicgstab ,cgs ,cbgmres ,gmres ,gcr ,idr" << std::endl;
+        std::cerr << "sover name: bicg ,bicgstab ,cgs ,cbgmres ,gmres ,gcr ,idr, cg, multigrid" << std::endl;
         std::exit(-1);
     }
 
@@ -172,6 +241,8 @@ int main(int argc, char* argv[])
         solve_system<gko::solver::Idr<ValueType> >(exec, A, b, x);
     else if (solver_name == "cg")
         solve_system<gko::solver::Cg<ValueType> >(exec, A, b, x);
+    // else if (solver_name == "multigrid")
+    //     solve_system_multigrid(exec, A, b, x, 10);
     else
     {
         std::cerr << "Unknown solver name: " << solver_name << std::endl;
