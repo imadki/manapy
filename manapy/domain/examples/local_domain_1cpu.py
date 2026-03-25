94ba9c0a5e4978cea8cc7bb4b8ef88175ea67a39
Ayoub ben hamou <ayoubbenhamou0731@gmail.com>
2026-03-25 23:35:14 +0100

    get access change in c_api

--- manapy/domain/examples/local_domain_1cpu.py
+++ manapy/domain/examples/local_domain_1cpu.py
@@ -3,13 +3,13 @@ from manapy.tests import LocalDomain1Cpu
 from manapy.tests.meshes import get_mesh
 
 # To change float type go to manapy/backends/types.py
-dim, mesh_path, mesh_name = get_mesh(5)
+dim, mesh_path, mesh_name = get_mesh(7)
 mesh = Mesh(mesh_path, dim)
 partitioning = Partitioning(mesh)
-nb_parts = 140
-partitioning.make_n_part_old(nb_parts)
+nb_parts = 100
+partitioning.make_n_part_mesh_nodal(nb_parts)
 local_domains = partitioning.create_sub_domains() # intermediate step
 
 
-ld = LocalDomain1Cpu.create_local_domains(local_domains) # list of local domains
+#ld = LocalDomain1Cpu.create_local_domains(local_domains) # list of local domains
 
