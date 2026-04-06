6ab1563a7e273d0824fa5aeb9a743d1a26a51c91
Ayoub ben hamou <ayoubbenhamou0731@gmail.com>
2026-04-06 18:17:07 +0100

    Raise an error if the domain is split into too many partitions, when MPI changes nb_parts.

--- manapy/examples/examples/mpi_create_domain.py
+++ manapy/examples/examples/mpi_create_domain.py
@@ -1,5 +1,6 @@
 from manapy.domain import Domain, Partitioning
 from manapy.helpers import get_mesh
 
-dim, mesh_path, mesh_name = get_mesh(5)
+# TODO fix when MPI change nb_Partitions
+dim, mesh_path, mesh_name = get_mesh(2)
 local_domain = Domain.create_domain(mesh_path, dim, Partitioning.Par_Nodal, recreate=True)
