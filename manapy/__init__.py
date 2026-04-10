bf3fafa60cef02c54d6bcc8729d55f9bf0994ff2
Ayoub ben hamou <ayoubbenhamou0731@gmail.com>
2026-04-10 15:38:45 +0100

    remove mgmetis and update installer

--- manapy/__init__.py
+++ manapy/__init__.py
@@ -1,8 +1,2 @@
 # -*- coding: UTF-8 -*-
-from .version    import __version__
-#from .ddm   import *
-#from .tools import *
-#from .fvm import *
-#from .comms import *
-#from .ast   import *
-#from .models import *
+from .version import __version__
