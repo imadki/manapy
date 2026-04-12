9eb344701c03de65191bb42512d1b111257f4f36
Ayoub ben hamou <ayoubbenhamou0731@gmail.com>
2026-04-12 13:19:12 +0100

    Refactor diffusion2d and shallow_water

---
+++ manapy/examples/examples/2D/shallow_water2d.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 16 09:13:21 2022
+
+@author: kissami
+"""
+
+from mpi4py import MPI
+from manapy.sys_solvers.shallowater.system import ShallowWaterSolver
+from manapy.sys_solvers.shallowater.tools_utils_compute import initialisation_SW
+import timeit
+from manapy.domain import Domain, Partitioning
+from manapy.helpers import get_mesh
+from manapy.core.Variable import Variable
+
+COMM = MPI.COMM_WORLD
+SIZE = COMM.Get_size()
+RANK = COMM.Get_rank()
+start = timeit.default_timer()
+
+dim, mesh_path, mesh_name = get_mesh("big/carre.msh")
+domain = Domain.create_domain(mesh_path, dim, Partitioning.Par_Nodal, recreate=True)
+faces = domain.faces
+cells = domain.cells
+halos = domain.halos
+nodes = domain.nodes
+
+nbnodes = domain.nbnodes
+nbfaces = domain.nbfaces
+nbcells = domain.nbcells
+
+end = timeit.default_timer()
+
+tt = COMM.reduce(end - start, op=MPI.MAX, root=0)
+if RANK == 0:
+  print("Time to create the domain", tt)
+
+# TODO tfinal
+if RANK == 0:
+  print("Start Computation ...")
+
+time = 0
+tfinal = .06
+miter = 0
+niter = 1
+saving_at_node = 1
+
+boundaries = {"in": "neumann",
+              "out": "neumann",
+              "upper": "nonslip",
+              "bottom": "nonslip"
+              }
+
+h = Variable(domain=domain)
+hu = Variable(domain=domain, BC=boundaries)
+hv = Variable(domain=domain, BC=boundaries)
+hc = Variable(domain=domain)
+Z = Variable(domain=domain)
+
+initialisation_SW(h.cell, hu.cell, hv.cell, hc.cell, Z.cell, cells.center, 0)
+
+S = ShallowWaterSolver(h=h, hvel=(hu, hv), hc=hc, Z=Z, order=2, cfl=0.8)
+
+ts = MPI.Wtime()
+if RANK == 0:
+  print("Start While loop ...")
+
+# loop over time
+while time < tfinal:
+
+  d_t = S.stepper()
+  tot = int(tfinal / d_t / 50) + 1
+
+  time = time + d_t
+
+  S.compute_fluxes()
+  S.compute_new_val()
+
+  if niter == 1 or niter % tot == 0:
+    if saving_at_node:
+      # save vtk files for the solution
+      h.update_halo_value()
+      h.update_ghost_value()
+      h.interpolate_celltonode()
+
+      # save vtk files for the solution
+      hu.update_halo_value()
+      hu.update_ghost_value()
+      hu.interpolate_celltonode()
+
+      # save vtk files for the solution
+      hv.update_halo_value()
+      hv.update_ghost_value()
+      hv.interpolate_celltonode()
+
+      domain.save_on_node_multi(d_t, time, niter, miter, variables=["h", "hu", "hv"],
+                                values=[h.node, hu.node, hv.node])
+    else:
+      domain.save_on_cell_multi(d_t, time, niter, miter, variables=["h", "hu", "hv"],
+                                values=[h.cell, hu.cell, hv.cell])
+    miter += 1
+
+  niter += 1
+
+te = MPI.Wtime()
+
+tt = COMM.reduce(te - ts, op=MPI.MAX, root=0)
+if RANK == 0:
+  print("Time to do calculation", tt)
+
