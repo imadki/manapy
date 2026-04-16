157a155bf569a0479d7fc5d7e87e3bc15ec17a8a
Ayoub ben hamou <ayoubbenhamou0731@gmail.com>
2026-04-16 19:10:47 +0100

    Update Docker file. Add some import packages

--- Docker/build_image.sh
+++ Docker/build_image.sh
@@ -2,5 +2,6 @@
 
 cd ../../
 docker build -t manapy:0.4 -f ./manapy/Docker/Dockerfile .
+# docker run -it --rm -v "$PWD:/workspace/manapy" manapy:0.4
 #sudo docker run --name manapy -d init manapy:0.4
 #sudo docker exec -it manapy /bin/bash
