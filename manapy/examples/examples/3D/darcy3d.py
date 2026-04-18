b712a940e9fd969fbe482408280e27412a67da2b
Ayoub ben hamou <ayoubbenhamou0731@gmail.com>
2026-04-18 10:41:37 +0100

    Add darcy3d, burgers3d, advection3d, diffusion3d and laplacien3d. Fix ls_compute for 3D.

--- manapy/examples/examples/3D/darcy3d.py
+++ manapy/examples/examples/3D/darcy3d.py
@@ -12,6 +12,7 @@ from manapy.domain import Domain, Partitioning
 from manapy.helpers import get_mesh
 from manapy.solvers.advecdiff.tools_utils_compute import initialisation_gaussian_3d
 from manapy.solvers.advecdiff.system import AdvectionDiffusionSolver
+from manapy.solvers.ls import PETScKrylovSolver
 from manapy.core.Variable import Variable
 
 COMM = MPI.COMM_WORLD
@@ -31,46 +32,72 @@ nbnodes = domain.nbnodes
 nbfaces = domain.nbfaces
 nbcells = domain.nbcells
 
-
-ts = MPI.Wtime()
-
 # TODO tfinal
 if RANK == 0: print("Start Computation ...")
 time = 0
-tfinal = .15
+tfinal = .25
 miter = 0
 niter = 1
 Pinit = 2.
 saving_at_node = 1
 
+boundaries = {"in": "dirichlet",
+              "out": "dirichlet",
+              "upper": "neumann",
+              "bottom": "neumann",
+              "front": "neumann",
+              "back": "neumann"
+              }
+values = {"in": Pinit,
+          "out": 0.,
+          }
+
 ne = Variable(domain=domain)
 u = Variable(domain=domain)
 v = Variable(domain=domain)
 w = Variable(domain=domain)
-P = Variable(domain=domain)  # , BC=boundaries, values=values)
+P = Variable(domain=domain, BC=boundaries, values_dict=values)
 
 # Call the transport solver
-S = AdvectionDiffusionSolver(ne, vel=(u, v, w), Dxx=0.01, Dyy=0., order=2, cfl=0.8)
+S = AdvectionDiffusionSolver(ne, vel=(u, v), Dxx=0., Dyy=0., order=2, cfl=0.8)
 
 ####Initialisation
 initialisation_gaussian_3d(ne.cell, u.cell, v.cell, w.cell, P.cell, cells.center, Pinit)
 f = lambda x, y, z: Pinit * (1. - x)
+
+###Linear sys confi###
+# If you want the default options please do conf = Struct()
+# reuse_mtx: matrix does not change during the while loop
+# scheme: diamond (fv4 not tested!!!)
+# verbose: printing the mumps/petsc output
+L = PETScKrylovSolver(domain=domain, var=P, reuse_mtx=True, scheme='diamond',
+              precond='gamg', sub_precond="amg",
+              eps_a=1e-10, eps_r=1e-10, method="gmres")
+
+ts = MPI.Wtime()
 
 if RANK == 0: print("Start While loop ...")
 
 # loop over time
 while time < tfinal:
 
-  u.face[:] = 2.
-  v.face[:] = 0.
-  w.face[:] = 0.
+  L()
+  P.update_halo_value()
+  P.update_ghost_value()
+  P.interpolate_celltonode()
+  L.compute_Sol_gradient()
+
+  # TODO -1
+  u.face[:] = P.gradfacex[:]
+  v.face[:] = P.gradfacey[:]
+  w.face[:] = P.gradfacez[:]
 
   u.interpolate_facetocell()
   v.interpolate_facetocell()
   w.interpolate_facetocell()
 
   d_t = S.stepper()
-  tot = int(tfinal / d_t / 10) + 1
+  tot = int(tfinal / d_t / 50) + 1
 
   time = time + d_t
 
@@ -99,18 +126,19 @@ while time < tfinal:
       w.update_ghost_value()
       w.interpolate_celltonode()
 
-      domain.save_on_node_multi(d_t, time, niter, miter, variables=["ne", "u", "v", "P"],
-                                values=[ne.node, u.node, v.node, P.node], file_format="vtu")
+      domain.save_on_node_multi(d_t, time, niter, miter, variables=["ne", "u", "v", "w", "P"],
+                                values=[ne.node, u.node, v.node, w.node, P.node])
     else:
-      domain.save_on_cell_multi(d_t, time, niter, miter, variables=["ne", "u", "v", "P"],
-                                values=[ne.cell, u.cell, v.cell, P.cell], file_format="vtu")
+      domain.save_on_cell_multi(d_t, time, niter, miter, variables=["ne", "u", "v", "w", "P"],
+                                values=[ne.cell, u.cell, v.cell, w.cell, P.cell])
     miter += 1
 
   niter += 1
 
 te = MPI.Wtime()
 
-
+tt = COMM.reduce(te - ts, op=MPI.MAX, root=0)
 if RANK == 0:
-  # End measuring energy after the function execution
-  tt = COMM.reduce(te - ts, op=MPI.MAX, root=0)
+  print("Time to do calculation", tt)
+
+# del L
