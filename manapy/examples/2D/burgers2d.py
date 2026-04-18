0eaed73dc69924d1705b364984034348f0f1595e
Ayoub ben hamou <ayoubbenhamou0731@gmail.com>
2026-04-18 10:44:27 +0100

    refactor examples

--- manapy/examples/2D/burgers2d.py
+++ manapy/examples/2D/burgers2d.py
@@ -5,56 +5,26 @@ Created on Wed Feb 16 09:13:21 2022
 
 @author: kissami
 """
-
 from mpi4py import MPI
 import timeit
+from manapy.domain import Domain, Partitioning
+from manapy.helpers import get_mesh
+from manapy.solvers.advec.tools_utils_compute import initialisation_gaussian_2d
+from manapy.solvers.advecdiff.system import AdvectionDiffusionSolver
+from manapy.core.Variable import Variable
 
 COMM = MPI.COMM_WORLD
 SIZE = COMM.Get_size()
 RANK = COMM.Get_rank()
-
-from manapy.partitions import MeshPartition
-from manapy.ddm import Domain
-
-from manapy.solvers.advecdiff.tools_utils import initialisation_gaussian_2d
-from manapy.solvers.advecdiff import AdvectionDiffusionSolver
-
-from manapy.ast import Variable
-from manapy.base.base import Struct
-
-import os
-
 start = timeit.default_timer()
 
-# ... get the mesh directory
-try:
-    MESH_DIR = os.environ['MESH_DIR']
- 
-except:
-    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-    BASE_DIR = os.path.join(BASE_DIR , '..', '..','..')
-    MESH_DIR = os.path.join(BASE_DIR, 'mesh')
- 
-filename = "rectangle.msh"
 
-#File name
-filename = os.path.join(MESH_DIR, filename)
-dim = 2
-
-###Config###
-#backend numba or python
-#signature: add types to functions (make them faster) but compilation take time
-#cache: avoid recompilation in the next run
-running_conf = Struct(backend="numba", signature=True, cache=True, precision="double")
-mesh = MeshPartition(filename, dim=dim, conf=running_conf, periodic=[0,0,0])
-
-#Create the informations about cells, faces and nodes
-domain = Domain(dim=dim, conf=running_conf)
+dim, mesh_path, mesh_name = get_mesh("big/carre1.msh")
+domain = Domain.create_domain(mesh_path, dim, Partitioning.Par_Nodal, recreate=True)
 faces = domain.faces
 cells = domain.cells
 halos = domain.halos
 nodes = domain.nodes
-
 
 nbnodes = domain.nbnodes
 nbfaces = domain.nbfaces
@@ -62,11 +32,11 @@ nbcells = domain.nbcells
 
 end = timeit.default_timer()
 
-tt = COMM.reduce(end -start, op=MPI.MAX, root=0)
+tt = COMM.reduce(end - start, op=MPI.MAX, root=0)
 if RANK == 0:
-    print("Time to create the domain", tt)
+  print("Time to create the domain", tt)
 
-#TODO tfinal
+# TODO tfinal
 if RANK == 0: print("Start Computation ...")
 time = 0
 tfinal = .25
@@ -75,83 +45,79 @@ niter = 1
 Pinit = 2.
 saving_at_node = 0
 
-boundaries = {"in" : "dirichlet",
-              "out" : "dirichlet",
-              "upper":"neumann",
-              "bottom":"neumann"
+boundaries = {"in": "dirichlet",
+              "out": "dirichlet",
+              "upper": "neumann",
+              "bottom": "neumann"
               }
-values = {"in" : Pinit,
+values = {"in": Pinit,
           "out": 0.,
           }
 
 ne = Variable(domain=domain)
-u  = Variable(domain=domain)
-v  = Variable(domain=domain)
-P  = Variable(domain=domain, BC=boundaries, values=values)
+u = Variable(domain=domain)
+v = Variable(domain=domain)
+P = Variable(domain=domain, BC=boundaries, values_dict=values)
 
-
-#Call the transport solver
-conf = Struct(Dxx=0.1, Dyy=0., order=2, cfl=0.8)
-S = AdvectionDiffusionSolver(ne, vel=(u, v), conf=conf)
+# Call the transport solver
+S = AdvectionDiffusionSolver(ne, vel=(u, v), Dxx=0.1, Dyy=0., order=2, cfl=0.8)
 
 ####Initialisation
 initialisation_gaussian_2d(ne.cell, u.cell, v.cell, P.cell, cells.center, Pinit)
-f = lambda x, y, z : Pinit * (1. - x)
+f = lambda x, y, z: Pinit * (1. - x)
 
 ts = MPI.Wtime()
 
 if RANK == 0: print("Start While loop ...")
 
-#loop over time
+# loop over time
 while time < tfinal:
-    
-    #TODO -1
-    u.face[:] = 2.
-    v.face[:] = 0.
-    
-    u.interpolate_facetocell()
-    v.interpolate_facetocell()
-    
-    d_t = S.stepper()
-    tot = int(tfinal/d_t/50)+1
 
-    time = time + d_t
-    
-    S.compute_fluxes()
-    S.compute_new_val()
-    
+  # TODO -1
+  u.face[:] = 2.
+  v.face[:] = 0.
 
-    if niter== 1 or niter%tot == 0:
-        if saving_at_node:
-            #save vtk files for the solution
-            ne.update_halo_value()
-            ne.update_ghost_value()  
-            ne.interpolate_celltonode()
-            
-            #save vtk files for the solution
-            u.update_halo_value()
-            u.update_ghost_value()  
-            u.interpolate_celltonode()
-            
-            #save vtk files for the solution
-            v.update_halo_value()
-            v.update_ghost_value()  
-            v.interpolate_celltonode()
-   
-    
-            domain.save_on_node_multi(d_t, time, niter, miter, variables=["ne", "u","v", "P"],
-                                      values=[ne.node, u.node,v.node, P.node])
-        else:
-            domain.save_on_cell_multi(d_t, time, niter, miter, variables=["ne", "u","v","P"],
-                                      values=[ne.cell, u.cell,v.cell, P.cell])
-        miter += 1
+  u.interpolate_facetocell()
+  v.interpolate_facetocell()
 
-    niter += 1
+  d_t = S.stepper()
+  tot = int(tfinal / d_t / 50) + 1
+
+  time = time + d_t
+
+  S.compute_fluxes()
+  S.compute_new_val()
+
+  if niter == 1 or niter % tot == 0:
+    if saving_at_node:
+      # save vtk files for the solution
+      ne.update_halo_value()
+      ne.update_ghost_value()
+      ne.interpolate_celltonode()
+
+      # save vtk files for the solution
+      u.update_halo_value()
+      u.update_ghost_value()
+      u.interpolate_celltonode()
+
+      # save vtk files for the solution
+      v.update_halo_value()
+      v.update_ghost_value()
+      v.interpolate_celltonode()
+
+      domain.save_on_node_multi(d_t, time, niter, miter, variables=["ne", "u", "v", "P"],
+                                values=[ne.node, u.node, v.node, P.node])
+    else:
+      domain.save_on_cell_multi(d_t, time, niter, miter, variables=["ne", "u", "v", "P"],
+                                values=[ne.cell, u.cell, v.cell, P.cell])
+    miter += 1
+
+  niter += 1
 
 te = MPI.Wtime()
 
-tt = COMM.reduce(te-ts, op=MPI.MAX, root=0)
+tt = COMM.reduce(te - ts, op=MPI.MAX, root=0)
 if RANK == 0:
-    print("Time to do calculation", tt)
+  print("Time to do calculation", tt)
 
-#del L
+# del L
