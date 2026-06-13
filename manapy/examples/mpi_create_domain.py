9aa9657f4442fe67b805bb971756c4d96053f0f2
imadki <kissami.imad@gmail.com>
2026-06-13 13:37:08 +0100

    Remove obsolete example scripts
    Drop examples/tmp/{laplacien,petsc_solver}.py (superseded by
    examples/2D/laplacien2d.py) and the standalone local_domain_1cpu.py /
    mpi_create_domain.py demos.

--- manapy/examples/mpi_create_domain.py
+++
@@ -1,6 +0,0 @@
-from manapy.domain import Domain, Partitioning
-from manapy.helpers import get_mesh
-
-# TODO fix when MPI change nb_Partitions
-dim, mesh_path, mesh_name = get_mesh("rectangles.msh")
-local_domain = Domain.create_domain(mesh_path, dim, Partitioning.Par_Nodal, recreate=True)
